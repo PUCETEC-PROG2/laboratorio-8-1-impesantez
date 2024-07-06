85626f5cf7c26c06b48f6dc9ad066261be85319b
impesantez <168923512+impesantez@users.noreply.github.com>
2024-07-06 12:50:37 -0500

    Add files via upload

---
+++ laboratorio-8-1-impesantez/pokedex/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Pokemon
+
+def index(request):
+    #pokemons = Pokemon.objects.all() 
+    pokemons = Pokemon.objects.order_by('type')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'pokemons': pokemons}, request))
+
+def pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    template = loader.get_template('display_pokemon.html')
+    context = {
+        'pokemon': pokemon
+    }
+    return HttpResponse(template.render(context, request))
