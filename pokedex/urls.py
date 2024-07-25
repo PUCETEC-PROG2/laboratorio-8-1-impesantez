c33fbf040baaa6ec885844632c3cb38e23eb0da3
Isa <impesantez@puce.edu.ec>
2024-07-25 18:27:16 -0500

    hola

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path 
 
 from . import views
 
+app_name = 'pokedex'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:pokemon>/", views.pokemon, name="pokemon"),
+    path("pokemon/<int:pokemon_id>/", views.pokemon, name="pokemon"), 
+    path("add_pokemon/", views.add_pokemon, name="add_pokemon"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
 ]
